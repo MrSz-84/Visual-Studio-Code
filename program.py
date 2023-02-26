3c5e921795f21999cc6e6c84d2364ead14e3e09d
MrSz-84 <pixelmotionhd@gmail.com>
2023-02-26 19:43:33 +0100

    Komments and variable input added

--- program.py
+++ program.py
@@ -1,6 +1,10 @@
 pomaranczowy_krol = 3
 agnieszka = 5
 adam = 6
-print(pomaranczowy_krol, agnieszka, adam, sep=',')
-pomaranczowy_razem = pomaranczowy_krol + agnieszka + adam
-print(pomaranczowy_razem)
+print(pomaranczowy_krol, agnieszka, adam, sep=', ')
+pomaranczy_razem = pomaranczowy_krol + agnieszka + adam
+print(pomaranczy_razem)
+
+print('Całkowita liczba pomarańczy:', pomaranczy_razem)
+
+print(pomaranczowy_krol ** agnieszka // adam)
--- temperature_converter.py
+++ temperature_converter.py
@@ -1,14 +1,21 @@
-celsius = 32.4
-fahrenheit = 68.9
-kelvin = 298.6
+# Variables input
+celsius = float(input("Input temperature in Centigrade: " ))
+fahrenheit = float(input("Input temperature in Fahrenheit scale: "))
+kelvin = float(input("Enter temperature in Kelvin scale: "))
 
+# Conversion calculations
 celsius_to_fahrenheit = ((celsius * (9/5)) + 32)
 fahrenheit_to_celsius = ((fahrenheit - 32) * (5/9))
-celsius_to_kelvin = celsius + 273.15
-kelvin_to_celsius = kelvin - 273.15
+celsius_to_kelvin = (celsius + 273.15)
+kelvin_to_celsius = (kelvin - 273.15)
+kelvin_to_fahrenheit = (((kelvin - 273.15) * 1.8) + 32)
+fahrenheit_to_kelvin = ((fahrenheit + 459.67) * (5/9))
 
+# Output data
 print("MrSz Corporation ©, Wroclaw 2023.", "All rights reserved. \n")
-print(celsius, "'C is", round(celsius_to_fahrenheit, 1), "'F")
-print(fahrenheit, "'F is", round(fahrenheit_to_celsius, 1), "'C")
-print(celsius, "'C is", round(celsius_to_kelvin, 1), "'K")
-print(kelvin, "'K is", round(kelvin_to_celsius, 1), "'C")
+print(celsius, "'C is:", round(celsius_to_fahrenheit, 1), "'F")
+print(fahrenheit, "'F is:", round(fahrenheit_to_celsius, 1), "'C")
+print(celsius, "'C is:", round(celsius_to_kelvin, 1), "'K")
+print(kelvin, "'K is:", round(kelvin_to_celsius, 1), "'C")
+print(kelvin, "'K is:", round(kelvin_to_fahrenheit, 2), "'F")
+print(fahrenheit, "'F is:", round(fahrenheit_to_kelvin, 2), "'K")
